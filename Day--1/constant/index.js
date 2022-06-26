40fa6aae2a9ba08ba81f187dd6bf7810587cb618
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-27 02:18:18 +0545

    Learning constants

---
+++ Day--1/constant/index.js
@@ -0,0 +1,7 @@
+//const function for a fixed value, cannot change later
+//throws error if you try to change value
+const primeNumber = 7;
+primeNumber=2;
+console.log(primeNumber)
+
+//results in error because 2 isn't a prime number.
