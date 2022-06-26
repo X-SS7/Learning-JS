db274ffa59bec51b78b6a35d0dd23b16a76adf4f
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-27 02:49:16 +0545

    Learning about var- functions in JS

---
+++ Day--1/primitive/index.js
@@ -0,0 +1,8 @@
+let name = "Sakshyam"; //string
+let age = 16; //Number
+let isApproved = true; //boolean literal
+let isDeclined = null; 
+let deliver = 1;
+
+//java script is totally dynamic language, no need to assign if string or integer.
+// all numbers are numbers
