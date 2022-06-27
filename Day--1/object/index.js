9b98d04cf1b61b42132238eee2936cca52d038a2
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-27 18:40:41 +0545

    Object

--- Day--1/object/index.js
+++ Day--1/object/index.js
@@ -3,4 +3,18 @@ let person ={
     age: 16
 };
 
+person.name = 'Saxam';
+
+person['name']= 'Jack'
+
 console.log(person);
+console.log(person.name); //shows name from the dot notation or the braket notation.
+
+// linked variable
+let apple = "banana";
+person['apple']= 'kera';
+console.log(person.apple);
+
+
+
+
