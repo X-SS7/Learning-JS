4858e038c421041e1252f189f98f67e8b184a2da
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-27 18:58:08 +0545

    Functions

---
+++ Day--1/functions/index.js
@@ -0,0 +1,12 @@
+//performing a task function
+function greet(name, lastname) {
+    console.log('Hello '+ name +' '+ lastname);
+}
+greet('Sakshyam', 'Sigdel');
+
+//calculative funtion
+function sqar(number) {
+    return number*number
+}
+
+console.log(sqar(56));
