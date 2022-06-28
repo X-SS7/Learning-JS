e978373632260ab6a682e1b59d1e5c8bdeef748a
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-28 23:47:01 +0545

    JS fundamentals

--- Day--2/Fundamentals from course/index.js
+++ Day--2/Fundamentals from course/index.js
@@ -1,3 +1,4 @@
+
 // if condition
 let js = 'phenomenal';
 if (js === 'phenomenal')
@@ -5,3 +6,53 @@ if (js === 'phenomenal')
 
 // basic calculator
 console.log(10 + 12 + 12 + 234 + 123);
+
+//value and variables...
+let firstName = 'saxam'; //firstName is a variable
+console.log(firstName);
+let PI = 3.1415;
+console.log(PI);
+
+// USE camelCase for easier intepretation.
+
+// types of Data in JS
+// Every value is an object or primitive;;
+// Number, String, Boolean, are the major data types
+
+let age = 16; //number variable
+let name = 'Sakshyam'; // string variable
+let ageCheck = true; //boolean variable
+
+// Dynamic typing, so chill
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof 23);
+console.log(typeof jsIsFun);
+console.log(typeof 'sakshyam');
+
+let abc;
+console.log(typeof abc); //undefined
+
+abc = "alphabet";
+console.log(typeof abc);
+console.log(typeof null);
+
+//mutateable variable 
+let age1 = 16;
+age1 = 16 + 1;
+console.log(age);
+
+// constant variable 
+const pi = 3.1415;
+console.log(pi);
+pi = 3 //error because pi is a predefined constant
+
+
+
+
+
+
+
+
