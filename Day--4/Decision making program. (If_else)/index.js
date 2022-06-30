be17252ab940e052fca09bde9c6c91d27b9d6ffb
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-30 08:58:31 +0545

    Decision making statements

---
+++ Day--4/Decision making program. (If_else)/index.js
@@ -0,0 +1,17 @@
+const age = 16;
+
+if (age >= 16) {
+    console.log("You are eligible to drive car😊🚗");
+}
+
+else {
+    console.log(`You need to wait for ${16 - age} years for getting drivers permit.`);
+}
+
+const birthYear = 2005
+let century;
+if (birthYear <= 2000) { century = "20th"; }
+else {
+    century = "21th";
+}
+console.log(century); 
