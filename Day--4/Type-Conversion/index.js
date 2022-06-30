1e777faf4589298f71d2ced1a989ad6a6409fa2c
X-SS7 <sakshyamsigdel@gmail.com>
2022-06-30 13:45:40 +0545

    type coercion

--- Day--4/Type-Conversion/index.js
+++ Day--4/Type-Conversion/index.js
@@ -1,5 +1,24 @@
 
 const inputYear = '1991';
-console.log(Number(inputYear));
 console.log(inputYear + 18);
 //result: 199118
+console.log(Number(inputYear) + 18);
+//result 2009
+
+console.log(Number('Sakshyam'));
+console.log(typeof NaN);
+
+const numberr = 1212121212;
+console.log(String(numberr));
+
+//Type coercion
+console.log('I am ' + 16 + ' years old');
+console.log('I am ' + String(16) + ' years old');
+
+console.log('22' - 100);
+console.log('22' + 100);
+
+//.WHAT? really intresting...
+
+
+
