0fc2e0c362f4d0e88909f321a468c384ce69a871
X-SS7 <sakshyamsigdel@gmail.com>
2022-07-07 17:59:54 +0545

    Ternary operators

---
+++ Day--8/index.js
@@ -0,0 +1,5 @@
+let age = 16;
+age >= 16 ? console.log('I like to drive Motorbike') : console.log('I like to ride bicycle');
+// ternary operator
+age = prompt("enter your age");
+console.log(`I like to ride ${age >= 16 ? 'Motorbike' : 'bicycle'}`);
